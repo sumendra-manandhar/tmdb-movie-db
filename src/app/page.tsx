4bcdb6c07e37dc78d9f40bf830eca2fcf1225c43
sumendra <sumendramanandhar@nchl.com.np>
2025-02-28 11:08:26 +0545

    Added Movie Category componet to render in view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/hero-section";
+import MovieSlider from "@/components/movie-slider";
 import Navbar from "@/components/navbar";
 import Image from "next/image";
 
@@ -7,6 +8,11 @@ export default function Home() {
     <main className="min-h-screen">
   <Navbar/>
   <HeroSection />
+  <div className="px-4 space-y-8 pb-8">
+        <MovieSlider title="Now Playing" type="now_playing" />
+        <MovieSlider title="Upcoming Movies" type="upcoming" />
+        <MovieSlider title="Top Rated" type="top_rated" />
+      </div>
     </main>
   );
 }
